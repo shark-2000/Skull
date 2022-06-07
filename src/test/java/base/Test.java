7f34c61e57d08d6f1b073e097bfefef4d1a175d0
shark-2000 <katyayans40@gmail.com>
2022-06-07 14:23:26 +0530

    packages

---
+++ src/test/java/base/Test.java
@@ -0,0 +1,20 @@
+package base;
+
+
+public class Test {
+//  @Test
+//  public void f() {
+//  }
+//  @AfterMethod
+//  public void afterMethod() {
+//  }
+//
+//  @BeforeClass
+//  public void beforeClass() {
+//  }
+//
+//  @AfterClass
+//  public void afterClass() {
+//  }
+
+}
